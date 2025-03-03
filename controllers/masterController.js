e5d005afbcf43866e9a62891a5148fb08296463e
SamThompson57 <s_thomo@hotmail.co.uk>
2025-03-03 12:51:13 +0000

    Started on the views and the new character form, Next: complete and test new character form

---
+++ controllers/masterController.js
@@ -0,0 +1,34 @@
+const db = require("../db/queries");
+
+//Get Character List
+async function characterListGet(req, res) {
+    const characters = await db.getAllCharacters();
+    res.send("Characters:" + characters.map(character => character.charactername))
+
+    res.render("characterList", {
+        title: "Character List",
+        characters: characters
+    })
+
+}
+
+//Get New Character
+async function newCharacterGet(req, res) {
+    res.render("newCharacter", {
+        title: "New Character"
+    })
+}
+
+//Post New Character
+async function newCharacterPost(req, res) {
+    const name = req.body.characterName;
+    const str = req.body.characterStr;
+    await db.insertCharacter(name,str);
+    res.redirect("/");
+}
+
+//Delete User
+
+module.exports = {
+    characterListGet
+}
